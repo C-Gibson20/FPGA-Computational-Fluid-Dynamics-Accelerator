4f64eaafcc66fa9ec04245056f455c4ace77e5aa
C-Gibson20 <cg.c0d3@gmail.com>
2025-06-13 22:40:49 +0100

    starting cache

--- hardware/rtl/DDR_pixel_out.v
+++ hardware/rtl/DDR_pixel_out.v
@@ -30,17 +30,16 @@ module BRAM_ctrl#(
     parameter  ADDRESS_WIDTH             = 12
     //parameter  C_m00_axis_tdata_WIDTH    = 144
 )(
-    input wire frame_ready,
     //take data of all 9 directions for each pixel at a time
-    input wire [15:0] n1,
-    input wire [15:0] null1,
-    input wire [15:0] ne1,
-    input wire [15:0] e1,
-    input wire [15:0] se1,
-    input wire [15:0] s1,
-    input wire [15:0] sw1,
-    input wire [15:0] w1,
-    input wire [15:0] nw1,
+    output wire [15:0] n1,
+    output wire [15:0] null1,
+    output wire [15:0] ne1,
+    output wire [15:0] e1,
+    output wire [15:0] se1,
+    output wire [15:0] s1,
+    output wire [15:0] sw1,
+    output wire [15:0] w1,
+    output wire [15:0] nw1,
     
     // AXI flags
     output reg [11:0] read_addr,
@@ -48,19 +47,18 @@ module BRAM_ctrl#(
     // Ports of Axi Master Bus Interface M00_AXIS
     input wire  m00_axis_aclk,
     input wire  m00_axis_aresetn,
-    output wire  m00_axis_tvalid,
-    output reg [144-1 : 0] m00_axis_tdata,
-    output wire [(144/8)-1 : 0] m00_axis_tstrb,
-    output wire  m00_axis_tlast,
-    input wire  m00_axis_tready
+    input wire  m00_axis_tvalid,
+    input wire [144-1 : 0] m00_axis_tdata,
+    input wire [(144/8)-1 : 0] m00_axis_tstrb,
+    input wire  m00_axis_tlast,
+    output wire  m00_axis_tready
 );
     
     //iterate through all 2500 pixels
-    reg [143:0] output_data;
+    reg [143:0] input_data;
     reg [1:0] current_state;
     reg [1:0] next_state;
     reg [5:0] count;
-    reg was_reset;
 
     //states
     localparam FILL_DATA    = 2'd0;
@@ -70,38 +68,27 @@ module BRAM_ctrl#(
     assign m00_axis_tstrb = {18{1'b1}};
 
     always @(posedge m00_axis_aclk or negedge m00_axis_aresetn) begin
-        if (!m00_axis_aresetn) begin
-            current_state <= IDLE;
-            was_reset <= 1;
-            read_addr <= 0;
+        if (m00_axis_aresetn) begin
+            current_state <= FILL_DATA;
         end
         else begin
             current_state <= next_state;
-            was_reset <= 0;
         end
-
     end
 ////////////////////////////////////////////////////////////////////////////
 
     //set flags etc here
     always @* begin
        case(current_state)
-        IDLE: begin
-            // read_addr = (was_reset) ? 0 : read_addr;
-            m00_axis_tlast = (was_reset) ? 0 : read_addr;
-            m00_axis_tvalid = (was_reset) ? 0 : read_addr;
-        end
-       
         FILL_DATA: begin
-            m00_axis_tvalid = 0;
+            tready = 1;
         end
         
         SEND: begin
-            m00_axis_tlast = (read_addr == DEPTH - 1);
+            tready = 0;
         end
 
-        default: begin
-        end
+        default: ;
        endcase
     end
 
@@ -111,45 +98,43 @@ module BRAM_ctrl#(
     //states
     always @* begin
         case(current_state)
-        IDLE: begin
-            if (m00_axis_tready && read_addr > 0) next_state = SEND;
-            else if (frame_ready && read_addr == 0) next_state = FILL_DATA;
-            else next_state = IDLE;
-        end
-
         FILL_DATA: begin
-            next_state = IDLE;
+            // tready is high
+            // tvalid is high
+            next_state = SEND;
         end
         
         SEND: begin
-            next_state = (m00_axis_tlast) ? IDLE : FILL_DATA;
-        end
-        
-        default: begin
-            next_state = IDLE;
-        end
-    endcase
-    end
-
-/////////////////////////////////////////////////////////////////
-
-    // state logic
-    always @(posedge m00_axis_aclk) begin
-        case(current_state)
-        FILL_DATA: begin
-            read_addr <= read_addr 1;
-            output_data <= {n1, null1, ne1, e1, se1, s1, sw1, w1, nw1};
-        end
-        
-        SEND: begin
-            // read_addr += 1;
-            m00_axis_tdata <= output_data;
+            //tready is low
+            next_state = FILL_DATA;
         end
         
         default: ;
     endcase
     end
 
+/////////////////////////////////////////////////////////////////
 
-
+    // state logic
+    always @* begin
+        case(current_state)
+        FILL_DATA: begin
+            input_data = m00_axis_tdata;
+        end
+        
+        SEND: begin
+            n1      = input_data[15:0],
+            null1   = input_data[31:16],
+            ne1     = input_data[47:32],
+            e1      = input_data[63:48],
+            se1     = input_data[79:64],
+            s1      = input_data[95:80],
+            sw1     = input_data[111:96],
+            w1      = input_data[127:112],
+            nw1     = input_data[143:128],
+        end
+        
+        default: ;
+    endcase
+    end
 endmodule
