b2adbeeae364c271eb2ac48b5d5639f5a0496e07
kayvan-faghani <kayfaghani@gmail.com>
2025-06-13 13:45:37 +0100

    This code is held together by ductape and dreams

--- hardware/rtl/topParallel.v
+++ hardware/rtl/topParallel.v
@@ -19,12 +19,12 @@ module topParallel (
     input wire [`DATA_WIDTH-1:0]        init_cw,
     input wire [`DATA_WIDTH-1:0]        init_cnw,
 
-    output wire signed [15:0] testing_cs_n_data_in, //for unit tests allowing me to test values for signals not exposed to the top layer
-    output wire signed [15:0] testing_c0_data_in,
-    output wire [`DATA_WIDTH-1:0] u_x,
-    output wire [`DATA_WIDTH-1:0] u_y,
-    output wire [`DATA_WIDTH-1:0] rho,
-    output wire [`DATA_WIDTH-1:0] u_squared,
+    output wire signed [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] testing_cs_n_data_in, //for unit tests allowing me to test values for signals not exposed to the top layer
+    output wire signed [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] testing_c0_data_in,
+    output wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] u_x,
+    output wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] u_y,
+    output wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] rho,
+    output wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] u_squared,
     output wire collider_ready,
     output wire in_collision_state
 );
@@ -35,15 +35,15 @@ module topParallel (
     wire [`ADDRESS_WIDTH-1:0] cs_addr, cs_n_addr, csw_addr, csw_n_addr;
     wire [`ADDRESS_WIDTH-1:0] cw_addr, cw_n_addr, cnw_addr, cnw_n_addr;
 
-    wire [`DATA_WIDTH-1:0] c0_data_in, c0_n_data_in, cn_data_in, cn_n_data_in, cne_data_in, cne_n_data_in;
-    wire [`DATA_WIDTH-1:0] ce_data_in, ce_n_data_in, cse_data_in, cse_n_data_in;
-    wire [`DATA_WIDTH-1:0] cs_data_in, cs_n_data_in, csw_data_in, csw_n_data_in;
-    wire [`DATA_WIDTH-1:0] cw_data_in, cw_n_data_in, cnw_data_in, cnw_n_data_in;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] c0_data_in, c0_n_data_in, cn_data_in, cn_n_data_in, cne_data_in, cne_n_data_in;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] ce_data_in, ce_n_data_in, cse_data_in, cse_n_data_in;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] cs_data_in, cs_n_data_in, csw_data_in, csw_n_data_in;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] cw_data_in, cw_n_data_in, cnw_data_in, cnw_n_data_in;
 
-    wire [`DATA_WIDTH-1:0] c0_data_out, c0_n_data_out, cn_data_out, cn_n_data_out, cne_data_out, cne_n_data_out;
-    wire [`DATA_WIDTH-1:0] ce_data_out, ce_n_data_out, cse_data_out, cse_n_data_out;
-    wire [`DATA_WIDTH-1:0] cs_data_out, cs_n_data_out, csw_data_out, csw_n_data_out;
-    wire [`DATA_WIDTH-1:0] cw_data_out, cw_n_data_out, cnw_data_out, cnw_n_data_out;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] c0_data_out, c0_n_data_out, cn_data_out, cn_n_data_out, cne_data_out, cne_n_data_out;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] ce_data_out, ce_n_data_out, cse_data_out, cse_n_data_out;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] cs_data_out, cs_n_data_out, csw_data_out, csw_n_data_out;
+    wire [`DATA_WIDTH*`RAMS_TO_ACCESS-1:0] cw_data_out, cw_n_data_out, cnw_data_out, cnw_n_data_out;
 
     wire c0_write_en, c0_n_write_en, cn_write_en, cn_n_write_en, cne_write_en, cne_n_write_en;
     wire ce_write_en, ce_n_write_en, cse_write_en, cse_n_write_en;
@@ -51,32 +51,32 @@ module topParallel (
     wire cw_write_en, cw_n_write_en, cnw_write_en, cnw_n_write_en;
 
     // Instantiate all 18 RAMs
-    RAM_2500 #(.INIT_FILE("ram0E02.mem")) c0     (.clk(clk), .addr(c0_addr),   .data_in(c0_data_in),   .write_en(c0_write_en),   .data_out(c0_data_out));
-    RAM_2500 #(.INIT_FILE("ram0E02.mem")) c0_n   (.clk(clk), .addr(c0_n_addr), .data_in(c0_n_data_in), .write_en(c0_n_write_en), .data_out(c0_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0E02.mem")) c0     (.clk(clk), .addr(c0_addr),   .data_in(c0_data_in),   .write_en(c0_write_en),   .data_out(c0_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0E02.mem")) c0_n   (.clk(clk), .addr(c0_n_addr), .data_in(c0_n_data_in), .write_en(c0_n_write_en), .data_out(c0_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram0381.mem")) cn ( .clk(clk), .addr(cn_addr   ), .data_in(cn_data_in   ), .write_en(cn_write_en   ), .data_out(cn_data_out   ) );
-    RAM_2500 #(.INIT_FILE("ram0381.mem")) cn_n   (.clk(clk), .addr(cn_n_addr), .data_in(cn_n_data_in), .write_en(cn_n_write_en), .data_out(cn_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0381.mem")) cn ( .clk(clk), .addr(cn_addr   ), .data_in(cn_data_in   ), .write_en(cn_write_en   ), .data_out(cn_data_out   ) );
+    RAM_2500_parallel #(.INIT_FILE("ram0381.mem")) cn_n   (.clk(clk), .addr(cn_n_addr), .data_in(cn_n_data_in), .write_en(cn_n_write_en), .data_out(cn_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram012F.mem")) cne    (.clk(clk), .addr(cne_addr),  .data_in(cne_data_in),  .write_en(cne_write_en),  .data_out(cne_data_out));
-    RAM_2500 #(.INIT_FILE("ram012F.mem")) cne_n  (.clk(clk), .addr(cne_n_addr),.data_in(cne_n_data_in),.write_en(cne_n_write_en),.data_out(cne_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram012F.mem")) cne    (.clk(clk), .addr(cne_addr),  .data_in(cne_data_in),  .write_en(cne_write_en),  .data_out(cne_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram012F.mem")) cne_n  (.clk(clk), .addr(cne_n_addr),.data_in(cne_n_data_in),.write_en(cne_n_write_en),.data_out(cne_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram04BB.mem")) ce     (.clk(clk), .addr(ce_addr),   .data_in(ce_data_in),   .write_en(ce_write_en),   .data_out(ce_data_out));
-    RAM_2500 #(.INIT_FILE("ram04BB.mem")) ce_n   (.clk(clk), .addr(ce_n_addr), .data_in(ce_n_data_in), .write_en(ce_n_write_en), .data_out(ce_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram04BB.mem")) ce     (.clk(clk), .addr(ce_addr),   .data_in(ce_data_in),   .write_en(ce_write_en),   .data_out(ce_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram04BB.mem")) ce_n   (.clk(clk), .addr(ce_n_addr), .data_in(ce_n_data_in), .write_en(ce_n_write_en), .data_out(ce_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram012F.mem")) cse    (.clk(clk), .addr(cse_addr),  .data_in(cse_data_in),  .write_en(cse_write_en),  .data_out(cse_data_out));
-    RAM_2500 #(.INIT_FILE("ram012F.mem")) cse_n  (.clk(clk), .addr(cse_n_addr),.data_in(cse_n_data_in),.write_en(cse_n_write_en),.data_out(cse_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram012F.mem")) cse    (.clk(clk), .addr(cse_addr),  .data_in(cse_data_in),  .write_en(cse_write_en),  .data_out(cse_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram012F.mem")) cse_n  (.clk(clk), .addr(cse_n_addr),.data_in(cse_n_data_in),.write_en(cse_n_write_en),.data_out(cse_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram0381.mem")) cs     (.clk(clk), .addr(cs_addr),   .data_in(cs_data_in),   .write_en(cs_write_en),   .data_out(cs_data_out));
-    RAM_2500 #(.INIT_FILE("ram0381.mem")) cs_n   (.clk(clk), .addr(cs_n_addr), .data_in(cs_n_data_in), .write_en(cs_n_write_en), .data_out(cs_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0381.mem")) cs     (.clk(clk), .addr(cs_addr),   .data_in(cs_data_in),   .write_en(cs_write_en),   .data_out(cs_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0381.mem")) cs_n   (.clk(clk), .addr(cs_n_addr), .data_in(cs_n_data_in), .write_en(cs_n_write_en), .data_out(cs_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram00A6.mem")) csw    (.clk(clk), .addr(csw_addr),  .data_in(csw_data_in),  .write_en(csw_write_en),  .data_out(csw_data_out));
-    RAM_2500 #(.INIT_FILE("ram00A6.mem")) csw_n  (.clk(clk), .addr(csw_n_addr),.data_in(csw_n_data_in),.write_en(csw_n_write_en),.data_out(csw_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram00A6.mem")) csw    (.clk(clk), .addr(csw_addr),  .data_in(csw_data_in),  .write_en(csw_write_en),  .data_out(csw_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram00A6.mem")) csw_n  (.clk(clk), .addr(csw_n_addr),.data_in(csw_n_data_in),.write_en(csw_n_write_en),.data_out(csw_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram0298.mem")) cw     (.clk(clk), .addr(cw_addr),   .data_in(cw_data_in),   .write_en(cw_write_en),   .data_out(cw_data_out));
-    RAM_2500 #(.INIT_FILE("ram0298.mem")) cw_n   (.clk(clk), .addr(cw_n_addr), .data_in(cw_n_data_in), .write_en(cw_n_write_en), .data_out(cw_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0298.mem")) cw     (.clk(clk), .addr(cw_addr),   .data_in(cw_data_in),   .write_en(cw_write_en),   .data_out(cw_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram0298.mem")) cw_n   (.clk(clk), .addr(cw_n_addr), .data_in(cw_n_data_in), .write_en(cw_n_write_en), .data_out(cw_n_data_out));
 
-    RAM_2500 #(.INIT_FILE("ram00A6.mem")) cnw    (.clk(clk), .addr(cnw_addr),  .data_in(cnw_data_in),  .write_en(cnw_write_en),  .data_out(cnw_data_out));
-    RAM_2500 #(.INIT_FILE("ram00A6.mem")) cnw_n  (.clk(clk), .addr(cnw_n_addr),.data_in(cnw_n_data_in),.write_en(cnw_n_write_en),.data_out(cnw_n_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram00A6.mem")) cnw    (.clk(clk), .addr(cnw_addr),  .data_in(cnw_data_in),  .write_en(cnw_write_en),  .data_out(cnw_data_out));
+    RAM_2500_parallel #(.INIT_FILE("ram00A6.mem")) cnw_n  (.clk(clk), .addr(cnw_n_addr),.data_in(cnw_n_data_in),.write_en(cnw_n_write_en),.data_out(cnw_n_data_out));
 
     assign testing_c0_data_in = c0_data_in;
     assign testing_cs_n_data_in = cs_data_in;
