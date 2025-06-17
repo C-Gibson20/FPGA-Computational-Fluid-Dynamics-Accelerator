aec66174105bcae8f66249871899304a89d8a050
Nishant <nishk8284@gmail.com>
2025-06-17 19:08:11 +0100

    edit maybe correct pls

--- drawing_software/receiver.py
+++ drawing_software/receiver.py
@@ -2,26 +2,25 @@ import socket
 import numpy as np
 from pynq import Overlay
 
-ol = Overlay()
-
-HOST = '192.168.2.99'
 PORT = 9005
+size = 2500
 
-overlay = Overlay('project_1.bit')
-AXI = ol.axi_gpio_0.channel1
+ol = Overlay('xxxxxxx.bit')
+AXI = ol.imgxxxx.channel1
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind(('0.0.0.0', PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
         while True:
-            data_buff = bytearray(conn.recv(156))  
-            for i in range(0, len(data_buff), 4):
-                word_bytes = data_buff[i:i+4]
-                if len(word_bytes) < 4:
+            data_buff = bytearray(conn.recv(np.ceil(size/8)))  
+            for i in range(0, size, 2):
+                word_bytes = data_buff[i:i+2]
+                if len(word_bytes) < 2:
                     continue  # skip incomplete word
                 word = int.from_bytes(word_bytes, byteorder='big') 
-                AXI.write((1 << 31) | (i << 16) | word) 
+                AXI.write((0 << 31) | (i << 16) | word, 0xFFFFFFFF)
+            AXI.write(1<<31,0x800000000) 
 conn.close()
 s.close()
