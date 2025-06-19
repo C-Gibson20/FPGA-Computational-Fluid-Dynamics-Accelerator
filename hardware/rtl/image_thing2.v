e6c7a305c6b561b322951ed453402a131c4387f9
Nishant <nishk8284@gmail.com>
2025-06-19 09:27:59 +0100

    eeeeee

--- hardware/rtl/image_thing2.v
+++ hardware/rtl/image_thing2.v
@@ -1,30 +1,81 @@
 `include "def.vh"
 
-module image_thing2 (
+module image_thing2 #(
+    parameter NUM_BLOCKS = 3,
+    parameter BLOCK_SIZE = 2500
+) (
     input wire [31:0] addr_din,
     input wire clk,
     output reg [2499:0] dout,
     output reg ram_addr, 
     output reg ram_wen, 
-    output reg ram_din
-    // this will use a RAM with a byte WEN
+    output reg [15:0] ram_din,
+    input reg [15:0] ram_dout,
+    input wire chunk_transfer_ready,
+    // this will use a RAM [2499:0] din and dout
 );
-// 
 
-reg [`DEPTH-1:0] buffer;
-wire send_img;
-wire [15:0] addr;
-wire [15:0] din;
-assign send_img = addr_din[31];
-assign addr = addr_din[30:16];
-assign din = addr_din[15:0];
+assign ram_addr = addr_din[30:16];
+assign ram_din = addr_din[15:0];  
+// assign ram_wen = 1;
+assign ram_wen = addr_din[31]; // MSB is WEN
 
+// this won't have the same nice behaviour as the double buffered
+
+localparam FILL_DATA = 1'd0;
+localparam OUT_RAM = 1'd1;
+
+reg [15:0] block_num;
+reg [15:0] index; // index within a given block
+
+reg current_state, next_state;
 
 always @(posedge clk) begin
-    if(send_img) begin
-        dout <= buffer;
-    end
-    buffer[addr +: 16] <= din;
+    current_state <= next_state;
 end
+
+// next state
+always @* begin
+    case (current_state)
+        FILL_DATA : begin
+            if(ram_wen) next_state = FILL_DATA;
+            else next_state = OUT_RAM;
+        end 
+        OUT_RAM : begin
+            if(index == BLOCK_SIZE && block_num == NUM_BLOCKS) 
+                next_state = FILL_DATA;
+            else
+                next_state = OUT_RAM;
+        end
+        default: next_state = current_state;
+    endcase
+end
+
+// behav
+
+always @(posedge clk) begin
+    if(current_state == FILL_DATA && next_state == OUT_RAM) begin
+        block_num <= 0;
+        index <= 0;
+    end
     
+end
+
+
+/// posedge detector ///////////
+reg sig_dly; 
+wire pe_ctr; //posedge of chunk transfer ready
+always @(posedge clk) begin
+    sig_dly <= chunk_transfer_ready; 
+end
+assign pe_ctr = chunk_transfer_ready & ~sig_dly;
+
+always @(posedge clk) begin
+    if(pe_ctr) block_num <= block_num + 1;
+end
+
+
+
+
+// get chunk
 endmodule
